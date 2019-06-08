73a4d2ac4c39958dd5ca278d844268d100c8a14f
Barak Dimand <barakdimand@aol.com>
2019-06-08 18:38:31 +0300

    trying to allow access if admin is entered as username

--- webstormProject/public/javascripts/functionality.js
+++ webstormProject/public/javascripts/functionality.js
@@ -8,5 +8,17 @@ function toSignIn() {
     window.location.replace('/');
 }
 
+function signIn(username) {
+    if (exists(username)) {
+        window.location.replace('store');
+    }
+    else alert("wrong username");
+}
+
+function exists(user) {
+    return user === "admin";
+}
+
 // when a new user registers and inputs all his info,
 // this will create a new user object and append the user to the users data structure
+
